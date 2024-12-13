909f02456ea4aea52d0b8cfe1ec36a679534c531
ELQUIS PACHECO <pachecoelquis@gmail.com>
2024-12-13 18:25:50 -0500

    delete config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -897,15 +897,3 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
-$settings['config_sync_directory'] = '../config/sync';$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'mariadb',
-  'port' => '3306',
-  'isolation_level' => 'READ COMMITTED',
-  'driver' => 'mysql',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
