3eb90be194c46e6f010255d16ab1369c3dd5cf73
ELQUIS PACHECO <pachecoelquis@gmail.com>
2025-01-22 09:50:36 -0500

    update button

--- web/themes/custom/basebook/components/02-molecules/card/card-image/card-image.stories.js
+++ web/themes/custom/basebook/components/02-molecules/card/card-image/card-image.stories.js
@@ -1,9 +1,9 @@
 import card from "./card-image.twig";
-
+import cardImage from "./card-image.yml";
 
 /**
  * Storybook Definition.
  */
 export default { title: "Molecules/Cards/card-image" };
 
-export const cardimage = () => card();
+export const cardimage = () => card(cardImage);
